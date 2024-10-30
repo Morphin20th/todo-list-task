94ecf3cff9a58ba2e19229f0b504139703bd6ff3
Maksym Bondarevskyi <bondarevskiymaks@gmail.com>
2024-10-30 21:23:47 +0200

    register models in admin.py

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from todo.models import Task, Tag
+
+admin.site.register(Tag)
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("content", "created_at", "deadline", "done")
+    list_filter = ("done", "tags")
+    search_fields = ("content",)
